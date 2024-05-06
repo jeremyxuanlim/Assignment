71c0da2f5cc5e61a80fe6d5c2bcbbcab53fdb65e
jeremyxuanlim <92426320+jeremyxuanlim@users.noreply.github.com>
2024-05-06 17:42:26 +0800

    Create payment successful.php

---
+++ assignment/payment successful.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Payment Page</title>
+    <link href="style/header.css" rel="stylesheet" />
+    <link href="style/footer.css" rel="stylesheet" />
+</head>
+<body>
+<?php include('includes/header.php'); ?>
+<h1>Payment Page</h1>
+<p>Payment Successful! Your ticket has been booked.</p>
+<?php include('includes/footer.php'); ?>
+</body>
+</html>
